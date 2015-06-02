4a39cd8b7e07ee8ee8213bfe4a24d6cac3258318
Grzegorz Chrupała <g.chrupala@uvt.nl>
2015-06-02 12:48:40 +0200

    Interface

--- example.py
+++ example.py
@@ -2,12 +2,14 @@
 from __future__ import division 
 import theano
 import numpy
-import json
 import random
 import itertools
-from layer import *
-from passage.preprocessing import Tokenizer
 import cPickle as pickle
+import argparse
+import gzip
+import sys
+import util
+from layer import *
 
 class EncoderDecoder(Layer):
     """A simple encoder-decoder net with shared input and output vocabulary."""
@@ -46,7 +48,9 @@ class Model(object):
         self.train = theano.function([self.input, self.output_prev, self.output ], 
                                       self.cost, updates=self.updates)
         self.predict = theano.function([self.input, self.output_prev], self.output_pred)
-    
+        # Like train, but no updates
+        self.loss = theano.function([self.input, self.output_prev, self.output ], self.cost)
+
 def sentences(data, split='train'):
     """Yields sentences from data."""
     for image in data:
@@ -66,35 +70,48 @@ def grouper(iterable, n):
     args = [iter(iterable)] * n
     return itertools.izip(*args)
 
+def batch(mapper, item):
+    """Prepare minibatch."""
+    mb = numpy.array(pad([[mapper.PAD_ID]+s+[mapper.END_ID] for s in item], mapper.PAD_ID), dtype='int32')
+    inp = mb[:,1:]
+    out = mb[:,1:]
+    out_prev = mb[:,0:-1]
+    return (inp, out_prev, out)
+
 def main():
-    data = json.load(open('/home/gchrupala/repos/neuraltalk/data/coco/dataset.json'))['images']
-    random.shuffle(data)
-    tokenizer = Tokenizer(min_df=10)
-    sents = list(sentences(data))
-    tokenized = tokenizer.fit_transform(sents)
-    PAD = tokenizer.encoder['PAD']
-    END = tokenizer.encoder['END']
+    parser = argparse.ArgumentParser(description="Stacked recurrent autoencoder for sentences.")
+    parser.add_argument('--size', type=int, default=512, help="Size of embeddings and hidden layers")
+    parser.add_argument('--depth', type=int, default=2, help="Number of hidden layers")
+    parser.add_argument('--epochs', type=int, default=1, help="Number of training epochs")
+    parser.add_argument('--seed', type=int, default=None, help="Random seed")
+    parser.add_argument('train', type=str, help="Path to training data")
+    parser.add_argument('valid', type=str, help="Path to validation data")
+    args = parser.parse_args()
+    mapper = util.IdMapper(min_df=10)
+    sents = mapper.fit_transform([ line.split() for line in open(args.train) ])
+    sents_valid = mapper.transform([line.split() for line in open(args.valid) ])
+    batch_valid = batch(mapper, sents_valid)
     mb_size = 128
-    print tokenizer.n_features
-    model = Model(size_vocab=tokenizer.n_features, size=512, depth=2)
-    for epoch in range(1,11):
+    model = Model(size_vocab=mapper.size(), size=args.size, depth=args.depth)
+    for epoch in range(1,args.epochs + 1):
         costs = 0 ; N = 0
-        for _j, item in enumerate(grouper(tokenized, 128)):
+        for _j, item in enumerate(grouper(sents, 128)):
             j = _j + 1
-            mb = numpy.array(pad([[PAD]+s+[END] for s in item], PAD), dtype='int32')
-            inp = mb[:,1:]
-            out = mb[:,1:]
-            out_prev = mb[:,0:-1]
+            inp, out_prev, out = batch(mapper, item)
             costs = costs + model.train(inp, out_prev, out) ; N = N + 1
-            print epoch, j, costs / N
+            print epoch, j, "train", costs / N
             if j % 50 == 0:
+                loss_valid = model.loss(*batch_valid)
+                print epoch, j, "valid", loss_valid
                 pred = model.predict(inp, out_prev)
                 for i in range(len(pred)):
-                    orig = tokenizer.inverse_transform([inp[i]])[0]
-                    res =  tokenizer.inverse_transform([numpy.argmax(pred, axis=2)[i]])[0]
-                    print orig
-                    print res
-        pickle.dump(model.network.params, open('params.{0}.pkl'.format(epoch),'w'))
+                    orig = mapper.inverse_transform([inp[i]])[0]
+                    res =  mapper.inverse_transform([numpy.argmax(pred, axis=2)[i]])[0]
+                    sys.stderr.write(orig)
+                    sys.stderr.write("\n")
+                    sys.stderr.write(res)
+                    sys.stderr.write("\n")
+        pickle.dump(model, gzip.open('model.{0}.pkl.gz'.format(epoch),'w'))
     
 if __name__ == '__main__':
     main()
