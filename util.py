04d011d04c375dab4460cd61aef026c1c180f379
Grzegorz Chrupała <pitekus@gmail.com>
2015-06-05 15:10:13 +0200

    autoassign and params in util

--- util.py
+++ util.py
@@ -154,3 +154,11 @@ class Adam(object):
         updates.append((i, i_t))
         return updates
 
+def autoassign(locs):
+    """Assign locals to self."""
+    for key in locs.keys():
+        if key!="self":
+            locs["self"].__dict__[key]=locs[key]
+
+def params(*layers):
+    return sum([ layer.params for layer in layers ], [])
