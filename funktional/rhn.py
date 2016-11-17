f6f38d6986bc9ad6c8bddf9e29a7dbafda5bbb5f
Grzegorz Chrupała <pitekus@gmail.com>
2016-11-17 16:50:06 +0100

    RHN

--- funktional/rhn.py
+++ funktional/rhn.py
@@ -3,37 +3,35 @@
 # by The Swiss AI lab IDSIA.
 import numpy as np
 import theano.tensor as tt
+import context
+from funktional.layer import Layer
 
-class Linear(Layer):
-    def __init__(self, in_size, out_size, bias, bias_init=None):
-        autoassign(locals())
-        assert bias == (bias_init is not None)
-        w = self.make_param((in_size, out_size), 'uniform')
-        y = tt.dot(x, w)
-        if bias:
-            b = self.make_param((out_size,), bias_init)
-            y += b
-        return y
+# class Linear(Layer):
+#     def __init__(self, in_size, out_size, bias, bias_init=None, init_scale=0.04):
+#         autoassign(locals())
+#         assert bias == (bias_init is not None)
+#         self.w = self.make_param((in_size, out_size), 'uniform')
+#         self.b = self.make_param((out_size,), bias_init)
     
-    def __call__(self, x):
-        y = tt.dot(x, self.w)
-        if self.bias:
-            y += self.b
-        return y
+#     def __call__(self, x):
+#         y = tt.dot(x, self.w)
+#         if self.bias:
+#             y += self.b
+#         return y
 
-    def params(self):
-        return [self.w, self.b]
+#     def params(self):
+#         return [self.w, self.b]
     
-    def make_param(self, shape, init_scheme):
-        """Create Theano shared variables, which are used as trainable model parameters."""
-        if isinstance(init_scheme, numbers.Number):
-            init_value = np.full(shape, init_scheme, floatX)
-        elif init_scheme == 'uniform':
-            init_value = self._np_rng.uniform(low=-self._init_scale, high=self._init_scale, size=shape).astype(floatX)
-        else:
-            raise AssertionError('unsupported init_scheme')
-        p = theano.shared(init_value)
-        return p
+#     def make_param(self, shape, init_scheme):
+#         """Create Theano shared variables, which are used as trainable model parameters."""
+#         if isinstance(init_scheme, numbers.Number):
+#             init_value = np.full(shape, init_scheme, floatX)
+#         elif init_scheme == 'uniform':
+#             init_value = np.uniform(low=-self.init_scale, high=self.init_scale, size=shape).astype(floatX)
+#         else:
+#             raise AssertionError('unsupported init_scheme')
+#         p = theano.shared(init_value)
+#         return p
 
     
 class RHN(Layer):
@@ -45,10 +43,12 @@ class RHN(Layer):
     def __init__(self, size_in, size, recur_depth=1, drop_i=0.75 , drop_s=0.25,
                  init_T_bias=-2.0, init_H_bias='uniform', tied_noise=True):
         autoassign(locals())
+        self._theano_rng = RandomStreams(config.seed // 2 + 321)
+        self._np_rng = np.random.RandomState(config.seed // 2 + 123)
         self._params = []
 
     def apply_dropout(self, x, noise):
-        return ifelse(self._is_training, noise * x, x)
+        return ifelse(context.training, noise * x, x)
 
     def get_dropout_noise(self, shape, dropout_p):
         keep_p = 1 - dropout_p
@@ -58,6 +58,26 @@ class RHN(Layer):
     def params(self):
         return self._params
 
+    def make_param(self, shape, init_scheme):
+        """Create Theano shared variables, which are used as trainable model parameters."""
+        if isinstance(init_scheme, numbers.Number):
+            init_value = np.full(shape, init_scheme, floatX)
+        elif init_scheme == 'uniform':
+            init_value = self._np_rng.uniform(low=-self._init_scale, high=self._init_scale, size=shape).astype(floatX)
+        else:
+            raise AssertionError('unsupported init_scheme')
+        p = theano.shared(init_value)
+        self._params.append(p)
+        return p
+
+    def linear(self, x, in_size, out_size, bias, bias_init=None):
+        assert bias == (bias_init is not None)
+        w = self.make_param((in_size, out_size), 'uniform')
+        y = tt.dot(x, w)
+        if bias:
+            b = self.make_param((out_size,), bias_init)
+            y += b
+        return y
    
     def step(self, i_for_H_t, i_for_T_t, h_tm1, noise_s):
         tanh, sigm = T.tanh, T.nnet.sigmoid
