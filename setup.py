bd286ba7cefda7aeb343ddc9cc53092d3673b8f4
Grzegorz Chrupała <pitekus@gmail.com>
2017-04-06 15:43:34 +0200

    Version

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 from setuptools import setup
 
 setup(name='funktional',
-      version='0.5',
+      version='0.6',
       description='A minimalistic toolkit for functionally composable neural network layers with Theano.',
       url='https://github.com/gchrupala/funktional',
       author='Grzegorz Chrupała',
       author_email='g.chrupala@uvt.nl',
       license='MIT',
       packages=['funktional'],
-      scripts=['autoencoder.py'],
       install_requires=['Theano'],
       zip_safe=False)
